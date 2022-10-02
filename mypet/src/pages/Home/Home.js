d0cfeeee47a4097a057097e939da9ca77bac1590
ofirc298 <61381462+ofirc298@users.noreply.github.com>
2022-10-02 13:39:27 +0300

    21022

--- mypet/src/pages/Home/Home.js
+++ mypet/src/pages/Home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import LogIn from '../connection/LogIn';
+import BusinessPage from '../Services/BusinessPage';
 
 function Home(props) {
     return (
         <div>
             home
+            {/* <LogIn></LogIn> */}
+            <BusinessPage/>
         </div>
     );
 }
---
+++ mypet/src/pages/Services/Services.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Services(props) {
+    return (
+        <div>
+            <div class="btn-group" role="group" aria-label="Basic example">
+                <button type="button" class="btn btn-primary">Left</button>
+                <button type="button" class="btn btn-primary">Middle1</button>
+                <button type="button" class="btn btn-primary">Middle2</button>
+                <button type="button" class="btn btn-primary">Right</button>
+            </div>
+        </div>
+    );
+}
+
+export default Services;
