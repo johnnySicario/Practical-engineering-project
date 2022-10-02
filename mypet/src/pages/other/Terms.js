32080fba9b64f67b8aaf18915db45085b2af89d4
ofirc298 <61381462+ofirc298@users.noreply.github.com>
2022-10-02 16:33:47 +0300

    terms

--- mypet/src/pages/other/Terms.js
+++ mypet/src/pages/other/Terms.js
@@ -3,29 +3,29 @@ import React from 'react';
 function Terms(props) {
     return (
         <div>
-        
-            ACCEPTANCE OF TERMS 
-By accessing or using of any of our Services,<br />
-including browsing our website (MyPet.com), <br />
-you accept these Terms. If you do not agree with the Terms,<br />
-you do not have the right to use our Services.<br />
-These Terms are a binding legal agreement between you and MyPet.com.<br />
-<p></p>
-ACCOUNT<br />
-All users must create an account with MyPet.com to use some of our Services. <br />
-To create and have an account with MyPet.com, you must:
-Be 18 years or older. <br />
-Minors (at least 13 years of age) are only permitted to use our Services through<br />
- the account of a parent or legal guardian with their permission and direct supervision.<br />
-Children under the age of 13 are not permitted to use our Services.<br />
- You are solely responsible for any activity conducted by a minor on your account.<br />
-Be a permanent resident of the United States.<br />
-Choose a user name that does not have language that is offensive, vulgar, <br />
-infringes the intellectual property of others, <br />
-or violates the Terms. Petstore.com reserves the right to reject a user name based <br />
-on Our subjective judgment on what constitutes a breach of this requirement.<br />
-Protect your password.
-Comply with all laws and ordinances applicable to your use of our Services.  
+
+            ACCEPTANCE OF TERMS
+            By accessing or using of any of our Services,<br />
+            including browsing our website (MyPet.com), <br />
+            you accept these Terms. If you do not agree with the Terms,<br />
+            you do not have the right to use our Services.<br />
+            These Terms are a binding legal agreement between you and MyPet.com.<br />
+            <p></p>
+            ACCOUNT<br />
+            All users must create an account with MyPet.com to use some of our Services. <br />
+            To create and have an account with MyPet.com, you must:
+            Be 18 years or older. <br />
+            Minors (at least 13 years of age) are only permitted to use our Services through<br />
+            the account of a parent or legal guardian with their permission and direct supervision.<br />
+            Children under the age of 13 are not permitted to use our Services.<br />
+            You are solely responsible for any activity conducted by a minor on your account.<br />
+            Be a permanent resident of the United States.<br />
+            Choose a user name that does not have language that is offensive, vulgar, <br />
+            infringes the intellectual property of others, <br />
+            or violates the Terms. Petstore.com reserves the right to reject a user name based <br />
+            on Our subjective judgment on what constitutes a breach of this requirement.<br />
+            Protect your password.
+            Comply with all laws and ordinances applicable to your use of our Services.
 
         </div>
     );
