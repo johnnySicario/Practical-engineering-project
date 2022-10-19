1ef049349edd8f13b95ab08b4b486eb44d483891
johnnySicario <jomieli19@gmail.com>
2022-10-19 13:00:53 +0300

    1

--- mypet/src/pages/site/Footer.js
+++ mypet/src/pages/site/Footer.js
@@ -28,7 +28,7 @@ const Footer = () => {
           </div>
 
           <div className="col-6 col-md-2 mb-3">
-            <h5>Section</h5>
+            <h5>Pet adoption</h5>
             <ul className="nav flex-column">
               <li style={{cursor : "pointer"}} className="nav-item mb-2"><a onClick={() => navigate('/Publication')} className="nav-link p-0 text-muted">Pet adoption</a></li>
               <li style={{cursor : "pointer"}} className="nav-item mb-2"><a onClick={() => navigate('/Service')} className="nav-link p-0 text-muted">Services</a></li>
