ad25e510e254f9826d4e3d7f10672b9f3516a999
Daniel Maximov <87207207+Danielmaximov123@users.noreply.github.com>
2022-10-19 11:59:36 +0300

    update

--- mypet/src/pages/Home/Home.js
+++ mypet/src/pages/Home/Home.js
@@ -3,7 +3,6 @@ import UserManagment from '../Administration/UserManagment.js';
 import Contact from '../other/Contact.js';
 import Main from '../Administration/User-management/Main/main.js';
 import Publication from '../Services/Publication.js';
-import Services from '../Services/Services.js';
 import TableServices from '../Services/TableServices.js';
 import SignUp from '../connection/SignUp.js';
 import { Navigate, Route, Routes } from 'react-router-dom';
