331e2b518d50d82acbb19201730cef3dca228338
Daniel Maximov <87207207+Danielmaximov123@users.noreply.github.com>
2022-10-19 16:24:10 +0300

    update 16:24

--- mypet/src/pages/Home/HomePage.js
+++ mypet/src/pages/Home/HomePage.js
@@ -2,10 +2,18 @@ import Carousel from "react-bootstrap/Carousel";
 import React, { useEffect, useState } from "react";
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 import Button from "react-bootstrap/esm/Button";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllPublications } from './../../redux/actions/getPublcationActions';
 
 const HomePageComp = () => {
   const token = useSelector((state) => state.users);
+  const dispatch = useDispatch()
+  const publications = useSelector(state => state.publication.publications)
+
+  useEffect(() => {
+    dispatch(getAllPublications())
+  },[dispatch])
+
 
   return (
     <div>
@@ -17,29 +25,20 @@ const HomePageComp = () => {
           width: "60%",
         }}
       >
-        <Carousel.Item interval={1500}>
-          <img
+        {publications.map((item , index) => {
+          return <Carousel.Item key={index}>
+            <img
             className="d-block w-100"
-            src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
-            alt="Image One"
-          />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            src={item.picture}
+            alt={item.header}
+            />
+            <Carousel.Caption style={{backgroundColor: 'rgba(0, 0, 0, 0.61)', padding: '2rem' , width : '50%' , marginLeft : "auto" , marginRight : "auto"}}>
+            <h3>{item.header}</h3>
+            <p>{item.text}</p>
             <Button>Go to Publication</Button>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item interval={500}>
-          <img
-            className="d-block w-100"
-            src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
-            alt="Image Two"
-          />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
+            </Carousel.Caption>
+          </Carousel.Item>
+        })}
       </Carousel>
     </div>
   );
