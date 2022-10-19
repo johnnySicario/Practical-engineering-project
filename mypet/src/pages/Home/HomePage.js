a7d718daec421e59fdce3921c8fc3a0002ace17b
Daniel Maximov <87207207+Danielmaximov123@users.noreply.github.com>
2022-10-19 12:01:04 +0300

    u

--- mypet/src/pages/Home/HomePage.js
+++ mypet/src/pages/Home/HomePage.js
@@ -1,80 +1,48 @@
-import Carousel from 'react-bootstrap/Carousel';
-import React, { useEffect, useState } from 'react'
+import Carousel from "react-bootstrap/Carousel";
+import React, { useEffect, useState } from "react";
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
-import './slider.css'
-import sliderData from './sliderData'
-import Button from 'react-bootstrap/esm/Button';
-import { useSelector } from 'react-redux';
-
+import Button from "react-bootstrap/esm/Button";
+import { useSelector } from "react-redux";
 
 const HomePageComp = () => {
-  const token = useSelector(state => state.users)
-
-    const [currentSlide, setCurrentSlide] = useState(0);
-    const slideLength = sliderData.length;
-  
-    const autoScroll = true;
-    let slideInterval;
-    let intervalTime = 5000;
-  
-    const nextSlide = () => {
-      setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
-      // console.log("next");
-    };
-  
-    const prevSlide = () => {
-      setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1);
-      console.log("prev");
-    };
-  
-    function auto() {
-      slideInterval = setInterval(nextSlide, intervalTime);
-    }
-  
-    useEffect(() => {
-      setCurrentSlide(0);
-    }, []);
-  
-    useEffect(() => {
-      if (autoScroll) {
-        auto();
-      }
-      return () => clearInterval(slideInterval);
-    }, [currentSlide]);
+  const token = useSelector((state) => state.users);
 
   return (
-    (
-      <div>
-              <Carousel style={{ opacity : 3 , position : 'relative' , marginLeft: '48rem'  , width : "60%" }}>
+    <div>
+      <Carousel
+        style={{
+          opacity: 3,
+          position: "relative",
+          marginLeft: "48rem",
+          width: "60%",
+        }}
+      >
         <Carousel.Item interval={1500}>
           <img
             className="d-block w-100"
-src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
+            src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
             alt="Image One"
           />
-                    <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          <Button>
-            Go to Publication
-          </Button>
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <Button>Go to Publication</Button>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={500}>
           <img
             className="d-block w-100"
-src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
+            src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
             alt="Image Two"
           />
           <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
-      </div>
-      )
-    )
-}
+    </div>
+  );
+};
 
-export default HomePageComp
+export default HomePageComp;
